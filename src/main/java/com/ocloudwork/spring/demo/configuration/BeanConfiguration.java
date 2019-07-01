1d334d25a65db0674ca46787d1d3d30b47f579b7
Administrator <Administrator@webservice.demo.com>
2019-07-01 15:38:28 +0800

    配置jackson

--- src/main/java/com/ocloudwork/spring/demo/configuration/BeanConfiguration.java
+++ src/main/java/com/ocloudwork/spring/demo/configuration/BeanConfiguration.java
@@ -4,11 +4,27 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+
 @Configuration
 public class BeanConfiguration {
 	
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+	
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module());
+		//自动搜索所有模块
+		objectMapper.findAndRegisterModules();
+		return objectMapper;
 	}
 }
